80ba4c9ab415eb5e5e82d23917b029dbe5638fa9
Lain0x0 <lainyamato03@gmail.com>
2024-04-07 17:42:48 +0000

    fizzbuzz fixed

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -17,14 +17,15 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
         else:
-            tmp_result.append(str(i))
+            if (i % 3) == 0:
+                tmp_result.append("Fizz")
+            elif (i % 5) == 0:
+                tmp_result.append("Buzz")
+            else:
+                tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
 
@@ -33,7 +34,7 @@ if __name__ == '__main__':
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
-        sys.exit()
+        sys.exit(1)
 
     number = int(sys.argv[1])
     fizzbuzz(number)
